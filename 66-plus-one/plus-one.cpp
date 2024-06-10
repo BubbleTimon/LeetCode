09f484c5b7b66e63b81205dd8ccc8db8b95d5453
BubbleTimon <95861392+BubbleTimon@users.noreply.github.com>
2024-06-10 15:24:59 +0900

    Update plus-one.cpp

--- 66-plus-one/plus-one.cpp
+++ 66-plus-one/plus-one.cpp
@@ -2,27 +2,15 @@ class Solution
 {
     public:
         vector<int> plusOne(vector<int> &digits) {
-            int sum = 0;
-            int exp = 0;
-            int num = 0;
-            int mod = 0;
-
-            vector<int> ret;
-
-            for (int i = digits.size() - 1; i >= 0; --i) {
-                if (i == digits.size() - 1)
-                    sum = digits[i] + 1;
-                else
-                    sum = digits[i] + exp;
-                
-                exp = sum / 10;
-                num = sum % 10;
-                ret.push_back(num);
+            int n = digits.size();
+            for (int i = n - 1; i >= 0; --i) {
+                if (digits[i] < 9) {
+                    digits[i]++;
+                    return digits;
+                }
+                digits[i] = 0;
             }
-            if (exp)
-                ret.push_back(exp);
-
-            reverse(ret.begin(), ret.end());
-            return ret;
+            digits.insert(digits.begin(), 1);
+            return digits;
         }
 };
