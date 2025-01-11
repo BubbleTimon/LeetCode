65241cd79a4e3e42392197ced5d6039905e52cf7
bubbletimon <95861392+BubbleTimon@users.noreply.github.com>
2025-01-11 17:09:01 +0900

    Time: 0 ms (100.00%) | Memory: 20.5 MB (81.93%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        map<int, int> m;
         int ret = 0;
 
         for (int i : nums)
             ret ^= i;
-            
+
         return ret;
     }
 };
