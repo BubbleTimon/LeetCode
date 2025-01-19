b576c8b39ab020bbb732a4d5d5dd70d8626cb65d
bubbletimon <95861392+BubbleTimon@users.noreply.github.com>
2025-01-19 18:15:08 +0900

    Time: 0 ms (100.00%) | Memory: 9.1 MB (11.30%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
     bool isValid(string s) {
-        bool ret = true;
         stack<char> st;
-
+        unordered_map<char, char> matching = {{')', '('}, {'}', '{'}, {']', '['}};
+        
         for (char& ch : s) {
-            if (ch == '(' || ch == '{' || ch == '[') st.push(ch);
-            else if (st.empty()) {
-                ret = false;
-                break;
-            }
-            else if (ch == ')' && st.top() == '(') st.pop();
-            else if (ch == '}' && st.top() == '{') st.pop();
-            else if (ch == ']' && st.top() == '[') st.pop();
-            else {
-                ret = false;
-                break;
+            if (matching.count(ch)) {  // 닫는 괄호일 경우
+                if (st.empty() || st.top() != matching[ch]) 
+                    return false;
+                st.pop();
+            } else {
+                st.push(ch);  // 여는 괄호일 경우
             }
         }
+        
+        return st.empty();  // 스택이 비어 있으면 유효한 괄호 문자열
 
-        if (st.empty() == false)
-            ret = false;
-            
-        return ret;
+        // bool ret = true;
+        // for (char& ch : s) {
+        //     if (ch == '(' || ch == '{' || ch == '[') st.push(ch);
+        //     else if (st.empty()) {
+        //         ret = false;
+        //         break;
+        //     }
+        //     else if (ch == ')' && st.top() == '(') st.pop();
+        //     else if (ch == '}' && st.top() == '{') st.pop();
+        //     else if (ch == ']' && st.top() == '[') st.pop();
+        //     else {
+        //         ret = false;
+        //         break;
+        //     }
+        // }
+        // return st.empty();
     }
 };
