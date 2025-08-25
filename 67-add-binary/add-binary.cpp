7ec24c5f77491f60aed773f97670a57a979aea7a
bubbletimon <95861392+BubbleTimon@users.noreply.github.com>
2025-08-25 14:57:50 +0900

    Time: 0 ms (100.00%) | Memory: 8.8 MB (82.10%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,33 +1,21 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        reverse(a.begin(), a.end());
-        reverse(b.begin(), b.end());
+        string ans;
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
 
-        if (a.size() < b.size())
-            swap(a, b);
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
 
-        while (a.size() != b.size())
-            b.push_back('0');
+            ans += (sum % 2) + '0';
+            carry = sum / 2;
+        }
 
-        string str = "";
-        int idx = 0;
-        int n = 0;
-        while (idx != a.size()) {
-            int now = a[idx] - '0' + b[idx] - '0' + n;
-            str.push_back((now % 2) + '0');
-            if (now >= 2)
-                n = 1;
-            else
-                n = 0;
-
-            idx++;
-        } 
-        
-        if (n == 1)
-            str.push_back('1');
-
-        reverse(str.begin(), str.end());
-        return str;
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
