ad55256267c419da19e433c474577a9073d4975a
bubbletimon <95861392+BubbleTimon@users.noreply.github.com>
2025-08-27 15:39:33 +0900

    Time: 0 ms (100.00%) | Memory: 8 MB (59.36%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<long long> dp(46);
+        dp[0] = 1, dp[1] = 2;
+        for (int i = 2; i < dp.size(); i++) {
+            dp[i] = dp[i - 2] + dp[i - 1];
+        }
+
+        return dp[n - 1];
+    }
+};
